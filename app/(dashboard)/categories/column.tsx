8b1c84d93be4cb0199797fb8ffd991223abc9d6e
MaxiiTech <129972796+MaxiiTech@users.noreply.github.com>
2024-06-30 10:58:11 +0100

    Update column.tsx

--- app/(dashboard)/categories/column.tsx
+++ app/(dashboard)/categories/column.tsx
@@ -12,7 +12,7 @@ import { Actions } from "./actions";
 export type ResponseType = InferResponseType<typeof client.api.categories.$get, 200>["data"][0];
 
 
-export const columns: ColumnDef<Payment>[] = [
+export const columns: ColumnDef<ResponseType>[] = [
   {
     id: "select",
     header: ({ table }) => (
